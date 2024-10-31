085f9c30b768eb826cc61df811253f3f8a5bd510
AthiseenChaiyod <152632490+AthiseenChaiyod@users.noreply.github.com>
2024-10-31 09:43:02 +0700

    chapter completed: host elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { ProjectionUsage } from './tutorials/1-components/8-content-projection-with-ng-content/projection-usage/projection-usage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-
-  imports: [ProjectionUsage],
 })
 export class AppComponent {}
