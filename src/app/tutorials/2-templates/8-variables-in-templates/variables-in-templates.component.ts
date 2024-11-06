7b4a7cf554c0b8d33c2bd14dfa4c64551775faaf
AthiseenChaiyod <152632490+AthiseenChaiyod@users.noreply.github.com>
2024-11-06 11:46:22 +0700

    chapter completed: variables in templates

---
+++ src/app/tutorials/2-templates/8-variables-in-templates/variables-in-templates.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: `variables-in-templates`,
+  templateUrl: `./variables-in-templates.component.html`,
+  styleUrl: `./variables-in-templates.component.css`,
+})
+export class VariablesInTemplates {}
