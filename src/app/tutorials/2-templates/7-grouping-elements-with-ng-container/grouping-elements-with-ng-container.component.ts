e3ec63822c9de643c5a2e5c9eb913bd3f886c4f9
AthiseenChaiyod <152632490+AthiseenChaiyod@users.noreply.github.com>
2024-11-06 11:26:42 +0700

    chapter completed: grouping elements with ng-container

---
+++ src/app/tutorials/2-templates/7-grouping-elements-with-ng-container/grouping-elements-with-ng-container.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: `grouping-elements-with-ng-container`,
+  templateUrl: `./grouping-elements-with-ng-container.component.html`,
+  styleUrl: `./grouping-elements-with-ng-container.component.css`,
+})
+export class GroupingElementsWithNgContainer {}
